8ccebd41b3ce5a317e20a7f51ec6aef539f20bfa
huncozyboy <leejh5838@naver.com>
2025-06-17 03:44:50 +0900

    CHORE : repo 초기설정

---
+++ README.md
@@ -0,0 +1,1 @@
+#LOKOKO-Server
---
+++ src/main/java/com/lokoko/LokokoApplication.java
@@ -0,0 +1,13 @@
+package com.lokoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokokoApplication.class, args);
+	}
+
+}
