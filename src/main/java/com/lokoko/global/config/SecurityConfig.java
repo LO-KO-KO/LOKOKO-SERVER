21d8061737f56cf04e3e92bc9e3b9b6ae962cb16
huncozyboy <leejh5838@naver.com>
2025-06-17 04:41:49 +0900

    #2 FEAT : SecurityConfig 구현 (일단 전체허용)

---
+++ src/main/java/com/lokoko/global/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.lokoko.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/*").permitAll()
+                .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
