2c54479f420d950b2cb1d0533c55d0a04cc26dd9
huncozyboy <leejh5838@naver.com>
2025-06-24 03:51:59 +0900

    MOD #6 : GeneratedValue 어노테이션 추가

--- src/main/java/com/lokoko/domain/user/entity/User.java
+++ src/main/java/com/lokoko/domain/user/entity/User.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -24,6 +26,7 @@ import lombok.experimental.SuperBuilder;
 public class User extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
