cdd2a5eae951037e1bb1c0737e3b3fedd1c42010
huncozyboy <leejh5838@naver.com>
2025-06-24 03:36:24 +0900

    DEL #6 : 중복된 예외처리 메세지 삭제

--- src/main/java/com/lokoko/global/auth/jwt/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/global/auth/jwt/exception/ErrorMessage.java
@@ -11,7 +11,6 @@ public enum ErrorMessage {
     JWT_TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다"),
     JWT_TOKEN_EXPIRED("만료된 토큰입니다."),
     JWT_TOKEN_INVALID("유효하지 않은 토큰 입니다."),
-    JWT_TOKEN_UN_VALID("유효하지 않은 토큰 입니다."),
     JWT_TOKEN_NOT_EXIST("헤더에 인증 토큰이 존재하지 않습니다"),
 
     COOKIE_NOT_FOUND("헤더에 RefreshToken이 없습니다."),
