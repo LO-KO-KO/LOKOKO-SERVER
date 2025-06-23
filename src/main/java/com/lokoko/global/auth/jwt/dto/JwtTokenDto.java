43f80090309336810e5f9e15fd91324f2d0cfacb
huncozyboy <leejh5838@naver.com>
2025-06-24 03:15:11 +0900

    FEAT #6 : 토큰 응답 DTO 추가

---
+++ src/main/java/com/lokoko/global/auth/jwt/dto/JwtTokenDto.java
@@ -0,0 +1,21 @@
+package com.lokoko.global.auth.jwt.dto;
+
+import lombok.Builder;
+
+@Builder
+public record JwtTokenDto(
+        String accessToken,
+        String refreshToken
+) {
+    public static JwtTokenDto of(String accessToken, String refreshToken) {
+        return JwtTokenDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken).build();
+    }
+
+    public static JwtTokenDto of(String accessToken) {
+        return JwtTokenDto.builder()
+                .accessToken(accessToken)
+                .build();
+    }
+}
