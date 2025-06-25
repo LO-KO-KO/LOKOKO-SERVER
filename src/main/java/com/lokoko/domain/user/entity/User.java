fc9151629383c81f3b0250c69c274a6dafbee541
huncozyboy <leejh5838@naver.com>
2025-06-25 09:55:44 +0900

    FEAT #9 : 유저 엔티티 수정 및 상태 추가

--- src/main/java/com/lokoko/domain/user/entity/User.java
+++ src/main/java/com/lokoko/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import com.lokoko.domain.user.entity.enums.PersonalColor;
 import com.lokoko.domain.user.entity.enums.Role;
 import com.lokoko.domain.user.entity.enums.SkinTone;
 import com.lokoko.domain.user.entity.enums.SkinType;
+import com.lokoko.domain.user.entity.enums.UserStatus;
 import com.lokoko.global.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,8 +35,11 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String lineId;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
+
+    @Column
+    private LocalDateTime lastLoginAt;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -52,7 +57,24 @@ public class User extends BaseEntity {
     @Column
     private SkinType skinType;
 
+    @Column(name = "status", nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status = UserStatus.ACTIVE;
+
     /*
      * TODO: 추후 scope 확장 시, 필드추가
      */
+
+    public static User createLineUser(String lineUserId) {
+        return User.builder()
+                .lineId(lineUserId)
+                .role(Role.USER)
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(LocalDateTime.now())
+                .build();
+    }
+
+    public void updateLastLoginAt() {
+        this.lastLoginAt = LocalDateTime.now();
+    }
 }
