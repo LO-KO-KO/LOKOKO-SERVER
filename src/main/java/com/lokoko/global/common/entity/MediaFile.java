be6f61f3bb36839033f8078988b63249dfcd4987
rookie <piresj0318@gmail.com>
2025-06-26 00:05:20 +0900

    FEAT #14 : 반복되는 파일 이름, 파일 경로 매핑을 위한 MediaFile 값 타입 객체 추가

---
+++ src/main/java/com/lokoko/global/common/entity/MediaFile.java
@@ -0,0 +1,27 @@
+package com.lokoko.global.common.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class MediaFile {
+
+	private String fileName;
+	private String fileUrl;
+
+	public MediaFile(String fileName, String fileUrl) {
+		this.fileName = fileName;
+		this.fileUrl = fileUrl;
+	}
+
+	public static MediaFile of(String fileName, String fileUrl) {
+		return new MediaFile(fileName, fileUrl);
+	}
+
+}
