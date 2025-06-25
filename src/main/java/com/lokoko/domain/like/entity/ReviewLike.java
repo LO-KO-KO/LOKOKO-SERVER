310466a76fdd6a302128341326b3dd6791e743ad
rookie <piresj0318@gmail.com>
2025-06-25 23:42:32 +0900

    FEAT #14 : 리뷰 좋아요 엔티티 구현

---
+++ src/main/java/com/lokoko/domain/like/entity/ReviewLike.java
@@ -0,0 +1,54 @@
+package com.lokoko.domain.like.entity;
+
+import com.lokoko.domain.review.entity.Review;
+import com.lokoko.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * 리뷰 좋아요
+ */
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+	@UniqueConstraint(name = "uk_review_like_review_user", columnNames = {"review_id", "user_id"})
+}) // (리뷰, 유저) 에 대해 유니크 제약조건 설정하여 같은 유저가 여러번 좋아요 누를 수 없도록 설정.
+public class ReviewLike {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_like_id")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "review_id") // foreign key
+	private Review review; // 어떤 리뷰에 대한 좋아요인지
+
+	@ManyToOne
+	@JoinColumn(name = "user_id") //  foreign key
+	private User user;  // 어떤 회원이 좋아요를 눌렀는지
+
+	// 정적 팩토리 메소드
+	public static ReviewLike createReviewLike(Review review, User user) {
+		return ReviewLike.builder()
+			.review(review)
+			.user(user)
+			.build();
+	}
+
+}
