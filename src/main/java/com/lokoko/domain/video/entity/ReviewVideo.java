3626f7a9db6b334124e7e52f7ca06d713e8640b3
rookie <piresj0318@gmail.com>
2025-06-26 00:06:43 +0900

    FEAT #14 : 리뷰 동영상 엔티티 구현

---
+++ src/main/java/com/lokoko/domain/video/entity/ReviewVideo.java
@@ -0,0 +1,50 @@
+package com.lokoko.domain.video.entity;
+
+import com.lokoko.domain.image.entity.ReceiptImage;
+import com.lokoko.domain.review.entity.Review;
+import com.lokoko.global.common.entity.MediaFile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewVideo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_video_id")
+	private Long id;
+
+	@Embedded
+	private MediaFile mediaFile; // 파일 이름, 파일 경로 포함
+
+	private int displayOrder; // 배치순서
+
+	@ManyToOne
+	@JoinColumn(name = "review_id")
+	private Review review; // 동영상이 어떤 리뷰에 대한 것인지 (외래키)
+
+	public static ReviewVideo createReviewVideo(String fileName,
+		String fileUrl, int displayOrder, Review review) {
+		return ReviewVideo.builder()
+			.mediaFile(MediaFile.of(fileName, fileUrl))
+			.displayOrder(displayOrder)
+			.review(review)
+			.build();
+	}
+
+
+}
