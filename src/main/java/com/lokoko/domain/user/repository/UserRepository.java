b28bf2be490baa94bbd3b8b98b5e63365a9027e3
huncozyboy <leejh5838@naver.com>
2025-06-25 09:56:53 +0900

    FEAT #9 : 유저 레포지토리 구현

---
+++ src/main/java/com/lokoko/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lokoko.domain.user.repository;
+
+import com.lokoko.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLineId(String lineId);
+}
