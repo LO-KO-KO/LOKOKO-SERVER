32cdcb2e664e13c59f5a843639cfddbff696aaf8
rookie <piresj0318@gmail.com>
2025-06-26 00:10:28 +0900

    MOD #14 : 영수증 이미지 필드 값 타입으로 변경
     Please enter the commit message for your changes. Lines starting

--- src/main/java/com/lokoko/domain/image/entity/ReceiptImage.java
+++ src/main/java/com/lokoko/domain/image/entity/ReceiptImage.java
@@ -1,7 +1,12 @@
 package com.lokoko.domain.image.entity;
 
-import com.lokoko.domain.review.entity.Review;
+import org.springframework.jdbc.support.MetaDataAccessException;
 
+import com.lokoko.domain.review.entity.Review;
+import com.lokoko.global.common.entity.MediaFile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,11 +26,11 @@ public class ReceiptImage {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "receipt_image_id")
 	private Long id;
 
-	private String fileName; // 영수증 이미지 파일 이름
-
-	private String fileUrl; // S3 상 영수증 이미지 파일 경로
+	@Embedded
+	private MediaFile mediaFile; // 파일 이름, 파일 경로 포함하는 값 타임 객체
 
 	private int displayOrder; // 배치순서
 
@@ -36,8 +41,7 @@ public class ReceiptImage {
 	public static ReceiptImage createReceiptImage(String fileName,
 		String fileUrl, int displayOrder, Review review) {
 		return ReceiptImage.builder()
-			.fileName(fileName)
-			.fileUrl(fileUrl)
+			.mediaFile(MediaFile.of(fileName, fileUrl))
 			.displayOrder(displayOrder)
 			.review(review)
 			.build();
