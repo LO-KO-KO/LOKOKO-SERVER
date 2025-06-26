cb1b4601399585bed59dc7b4182841f80c942ecd
huncozyboy <leejh5838@naver.com>
2025-06-27 03:37:44 +0900

    FEAT #13 : 제품, 제품 이미지 엔티티 추가

---
+++ src/main/java/com/lokoko/domain/product/entity/Product.java
@@ -0,0 +1,73 @@
+package com.lokoko.domain.product.entity;
+
+import com.lokoko.domain.product.entity.enums.MainCategory;
+import com.lokoko.domain.product.entity.enums.MiddleCategory;
+import com.lokoko.domain.product.entity.enums.SubCategory;
+import com.lokoko.domain.product.entity.enums.Tag;
+import com.lokoko.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private long normalPrice;
+
+    @Column(nullable = false)
+    private String brandName;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private String shippingInfo;
+
+    @Column(columnDefinition = "TEXT")
+    private String productDetail;
+
+    @Column(columnDefinition = "TEXT")
+    private String ingredients;
+
+    @Column(columnDefinition = "TEXT")
+    private String youtubeUrl;
+
+    @Column
+    private String oliveYoungUrl;
+
+    @Column
+    private String qoo10Url;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Tag tag;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private MainCategory mainCategory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private MiddleCategory middleCategory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private SubCategory subCategory;
+}
