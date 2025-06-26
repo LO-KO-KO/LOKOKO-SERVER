f6d6de755810e05be78f28a0f2783996e9a55292
huncozyboy <leejh5838@naver.com>
2025-06-27 03:39:40 +0900

    FEAT #13 : 크롤링 관련 Config 설정

---
+++ src/main/java/com/lokoko/global/config/SeleniumConfig.java
@@ -0,0 +1,18 @@
+package com.lokoko.global.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeleniumConfig {
+
+    @Bean
+    public WebDriver webDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--lang=ja-JP");
+        return new ChromeDriver(options);
+    }
+}
