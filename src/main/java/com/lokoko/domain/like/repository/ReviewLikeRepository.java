3c45fa47d9398db2351da9ce920c791f14007e3e
rookie <piresj0318@gmail.com>
2025-06-27 14:23:24 +0900

    MOD #14 : 레포지토리 인터페이스에 @Repository 애노테이션 추가
    프로젝트의 컨벤션을 통일하는 목적과, DataAccessException 예외 관리 등을 위해
    해당 애노테이션을 생략하지 않고 명시하기로 결정하였습니다.

--- src/main/java/com/lokoko/domain/like/repository/ReviewLikeRepository.java
+++ src/main/java/com/lokoko/domain/like/repository/ReviewLikeRepository.java
@@ -2,6 +2,8 @@ package com.lokoko.domain.like.repository;
 
 import com.lokoko.domain.like.entity.ReviewLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
 }
--- src/main/java/com/lokoko/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/lokoko/domain/review/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.lokoko.domain.review.repository;
 
 import com.lokoko.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
