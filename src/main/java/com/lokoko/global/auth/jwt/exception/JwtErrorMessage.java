1d09c2bee3c5ef71a01f50898c4ae83c416938d4
huncozyboy <leejh5838@naver.com>
2025-06-28 02:20:13 +0900

    FIX #18 : JWT 에러처리시, 매핑 관련 오류 삭제

--- src/main/java/com/lokoko/global/auth/jwt/exception/JwtErrorMessage.java
+++ src/main/java/com/lokoko/global/auth/jwt/exception/JwtErrorMessage.java
@@ -2,19 +2,18 @@ package com.lokoko.global.auth.jwt.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum JwtErrorMessage {
-    USER_NOT_FOUND_EMAIL("해당 이메일의 유저를 찾을 수 없습니다"),
-    JWT_TOKEN_FORBIDDEN("권한이 없습니다."),
-    JWT_TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다"),
-    JWT_TOKEN_EXPIRED("만료된 토큰입니다."),
-    JWT_TOKEN_INVALID("유효하지 않은 토큰 입니다."),
-    JWT_TOKEN_NOT_EXIST("헤더에 인증 토큰이 존재하지 않습니다"),
+    JWT_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다"),
+    JWT_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    JWT_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 입니다."),
 
-    COOKIE_NOT_FOUND("헤더에 RefreshToken이 없습니다."),
-    REDIS_NOT_FOUND("Redis 에서 찾을 수 없습니다.");
+    COOKIE_NOT_FOUND(HttpStatus.BAD_REQUEST, "헤더에 RefreshToken이 없습니다."),
+    REDIS_NOT_FOUND(HttpStatus.NOT_FOUND, "Redis에서 찾을 수 없습니다.");
 
+    private final HttpStatus status;
     private final String message;
 }
