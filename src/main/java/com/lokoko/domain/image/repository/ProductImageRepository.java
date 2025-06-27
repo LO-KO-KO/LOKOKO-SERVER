47663916e3bb52db46e6a21d86742f994c646405
rookie <piresj0318@gmail.com>
2025-06-27 21:23:20 +0900

    FEAT #19 : 레포지토리에서 제품 검색 및 리뷰 카운트 로직 구현

--- src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
@@ -1,7 +1,10 @@
 package com.lokoko.domain.image.repository;
 
 import com.lokoko.domain.image.entity.ProductImage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+
+    List<ProductImage> findByProductIdin(List<Long> productIds);
 }
