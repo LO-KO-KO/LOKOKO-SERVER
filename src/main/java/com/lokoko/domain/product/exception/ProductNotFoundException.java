6a7af5531d69d1aacc0068da8b4d293c259d29cd
rookie <piresj0318@gmail.com>
2025-06-27 21:27:01 +0900

    MOD #19 : 예외 설명 추가

--- src/main/java/com/lokoko/domain/product/exception/ProductNotFoundException.java
+++ src/main/java/com/lokoko/domain/product/exception/ProductNotFoundException.java
@@ -3,6 +3,7 @@ package com.lokoko.domain.product.exception;
 import com.lokoko.global.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
+//ProductNotFoundException 은, product id 로 제품을 검색했을 때, 해당하는 제품이 없는 경우 던지는 예외입니다.
 public class ProductNotFoundException extends BaseException {
 
     public ProductNotFoundException(String message) {
