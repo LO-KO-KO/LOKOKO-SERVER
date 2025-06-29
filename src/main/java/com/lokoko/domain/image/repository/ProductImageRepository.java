3425e9dd90bf6a66730718d38cdfc651e4877fde
rookie <piresj0318@gmail.com>
2025-06-29 21:47:05 +0900

    MOD #19 : 메소드 오타 수정

--- src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
-    List<ProductImage> findByProductIdin(List<Long> productIds);
+    List<ProductImage> findByProductIdIn(List<Long> productIds);
 }
