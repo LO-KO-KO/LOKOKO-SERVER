b91df545216ad59806ecd33a50aab16d2fba84c8
rookie <piresj0318@gmail.com>
2025-06-30 00:44:04 +0900

    MOD #19 : ProductImage 레포지토리에 누락된 @Repository 애노테이션 추가

--- src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ProductImageRepository.java
@@ -3,7 +3,9 @@ package com.lokoko.domain.image.repository;
 import com.lokoko.domain.image.entity.ProductImage;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
     List<ProductImage> findByProductIdIn(List<Long> productIds);
