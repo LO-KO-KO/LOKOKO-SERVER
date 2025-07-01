73b53e022554608868c1fb0bdda346dca109f161
huncozyboy <leejh5838@naver.com>
2025-07-01 10:45:23 +0900

    FEAT #12 : Line 소셜로그인 응답, 필드에 email 추가

--- src/main/java/com/lokoko/global/utils/LineConstants.java
+++ src/main/java/com/lokoko/global/utils/LineConstants.java
@@ -14,7 +14,7 @@ public class LineConstants {
     public static final String PARAM_REDIRECT_URI = "&redirect_uri=";
     public static final String PARAM_CLIENT_ID = "&client_id=";
     public static final String PARAM_CLIENT_SECRET = "client_secret";
-    public static final String PARAM_SCOPE = "&scope=profile%20openid";
+    public static final String PARAM_SCOPE = "&scope=profile%20openid%20email";
     public static final String PARAM_STATE = "&state=";
     public static final String RESPONSE_TYPE_CODE = "code";
     public static final String GRANT_TYPE_AUTH_CODE = "authorization_code";
