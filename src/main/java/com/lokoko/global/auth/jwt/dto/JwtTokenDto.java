ff95bd06478e43b133cb4a0ad7cf3bf4ccd6419b
huncozyboy <leejh5838@naver.com>
2025-07-01 12:50:01 +0900

    MOD #12 : TokenDto, LoginDto 분리

--- src/main/java/com/lokoko/global/auth/jwt/dto/JwtTokenDto.java
+++ src/main/java/com/lokoko/global/auth/jwt/dto/JwtTokenDto.java
@@ -1,23 +1,14 @@
 package com.lokoko.global.auth.jwt.dto;
 
-import com.lokoko.global.auth.entity.enums.OauthLoginStatus;
 import lombok.Builder;
 
 @Builder
 public record JwtTokenDto(
         String accessToken,
         String refreshToken,
-        OauthLoginStatus loginStatus
+        String tokenId
 ) {
-    public static JwtTokenDto of(
-            String accessToken,
-            String refreshToken,
-            OauthLoginStatus loginStatus
-    ) {
-        return new JwtTokenDto(accessToken, refreshToken, loginStatus);
-    }
-
-    public static JwtTokenDto of(String accessToken, String refreshToken) {
-        return new JwtTokenDto(accessToken, refreshToken, OauthLoginStatus.LOGIN);
+    public static JwtTokenDto of(String accessToken, String refreshToken, String tokenId) {
+        return new JwtTokenDto(accessToken, refreshToken, tokenId);
     }
 }
