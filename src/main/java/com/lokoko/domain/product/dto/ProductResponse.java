2dad3ec5ddea313848bf727300eb541f9523ddd9
rookie <piresj0318@gmail.com>
2025-07-03 01:21:07 +0900

    FEAT #32 : ProductResponse DTO 에 별점 항목 추가

--- src/main/java/com/lokoko/domain/product/dto/ProductResponse.java
+++ src/main/java/com/lokoko/domain/product/dto/ProductResponse.java
@@ -5,7 +5,8 @@ public record ProductResponse(
         Long productId, // 제품 id(추후 상세조회를 위해서)
         String imageUrl, // 제품 이미지
         String productName, // 제품 이름
-        Long reviewCount // 리뷰 개수
+        Long reviewCount, // 리뷰 개수
+        Double rating // 별점
 ) {
 
 
