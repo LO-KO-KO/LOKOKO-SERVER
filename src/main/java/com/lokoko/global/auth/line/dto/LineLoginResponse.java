95c0e37e4ca6ec0589c122ac95db7cb08adc792b
huncozyboy <leejh5838@naver.com>
2025-07-03 02:42:16 +0900

    MOD #33 : 라인 소셜 로그인 응답 dto 수정

--- src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
+++ src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
@@ -1,8 +1,8 @@
 package com.lokoko.global.auth.line.dto;
 
-import com.lokoko.global.auth.jwt.dto.LoginDto;
+import com.lokoko.global.auth.entity.enums.OauthLoginStatus;
 
 public record LineLoginResponse(
-        LoginDto tokens
+        OauthLoginStatus loginStatus
 ) {
 }
