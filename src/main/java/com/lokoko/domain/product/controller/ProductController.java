e132541a055d82034b639f4f0b81940141ec2790
huncozyboy <leejh5838@naver.com>
2025-07-05 22:26:39 +0900

    FEAT #36 : 신제품 카테고리별 조회 API 컨트롤러단 추가

--- src/main/java/com/lokoko/domain/product/controller/ProductController.java
+++ src/main/java/com/lokoko/domain/product/controller/ProductController.java
@@ -1,16 +1,18 @@
 package com.lokoko.domain.product.controller;
 
 
+import static com.lokoko.domain.product.controller.enums.ResponseMessage.CATEGORY_NEW_LIST_SUCCESS;
 import static com.lokoko.domain.product.controller.enums.ResponseMessage.CATEGORY_SEARCH_SUCCESS;
 
 import com.lokoko.domain.product.controller.enums.ResponseMessage;
+import com.lokoko.domain.product.dto.CategoryNewProductResponse;
 import com.lokoko.domain.product.dto.CategoryProductResponse;
 import com.lokoko.domain.product.dto.CrawlRequest;
 import com.lokoko.domain.product.dto.NameBrandProductResponse;
 import com.lokoko.domain.product.dto.ProductSearchRequest;
 import com.lokoko.domain.product.service.NewProductCrawlingService;
 import com.lokoko.domain.product.service.ProductCrawlingService;
-import com.lokoko.domain.product.service.ProductService;
+import com.lokoko.domain.product.service.ProductReadService;
 import com.lokoko.global.common.response.ApiResponse;
 import com.lokoko.global.kuromoji.service.ProductMigrationService;
 import io.swagger.v3.oas.annotations.Hidden;
@@ -31,8 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
-
-    private final ProductService productService;
+    private final ProductReadService productReadService;
     private final ProductCrawlingService productCrawlingService;
     private final NewProductCrawlingService newProductCrawlingService;
     private final ProductMigrationService productMigrationService;
@@ -51,12 +52,19 @@ public class ProductController {
     @GetMapping("/categories/search")
     public ApiResponse<CategoryProductResponse> searchProductsByCategory(@RequestParam String middleCategoryId,
                                                                          @RequestParam(required = false) String subCategoryId) {
-
-        CategoryProductResponse categoryProductResponse = productService.searchProductsByCategory(middleCategoryId,
+        CategoryProductResponse categoryProductResponse = productReadService.searchProductsByCategory(middleCategoryId,
                 subCategoryId);
 
-        return ApiResponse.success(HttpStatus.OK, CATEGORY_SEARCH_SUCCESS.getMessage(),
-                categoryProductResponse);
+        return ApiResponse.success(HttpStatus.OK, CATEGORY_SEARCH_SUCCESS.getMessage(), categoryProductResponse);
+    }
+
+    @Operation(summary = "신상품 카테고리별 조회")
+    @GetMapping("/categories/new")
+    public ApiResponse<CategoryNewProductResponse> searchNewProductsByCategory(@RequestParam String middleCategoryId) {
+        CategoryNewProductResponse categoryNewProductResponse = productReadService.searchNewProductsByCategory(
+                middleCategoryId);
+
+        return ApiResponse.success(HttpStatus.OK, CATEGORY_NEW_LIST_SUCCESS.getMessage(), categoryNewProductResponse);
     }
 
     @Hidden
