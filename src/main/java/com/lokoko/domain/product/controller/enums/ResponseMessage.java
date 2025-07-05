e482104355b9a8f7d2e59ce9b2ab8a872a5780c1
huncozyboy <leejh5838@naver.com>
2025-07-06 00:14:40 +0900

    FEAT #31 : 상세조회 유튜브 리뷰 URL 조회 성공응답 구현

--- src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
@@ -12,7 +12,8 @@ public enum ResponseMessage {
     PRODUCT_CRAWL_NEW_SUCCESS("신상품 크롤링에 성공했습니다."),
     PRODUCT_OPTION_SUCCESS("상품 옵션 크롤링에 성공했습니다."),
     CATEGORY_NEW_LIST_SUCCESS("신상품 카테고리 리스트 반환에 성공했습니다."),
-    PRODUCT_DETAIL_SUCCESS("상세조회 (제품관련) 조회에 성공했습니다.");
+    PRODUCT_DETAIL_SUCCESS("상세조회 (제품관련) 조회에 성공했습니다."),
+    PRODUCT_YOUTUBE_DETAIL_SUCCESS("상세조회 (유튜브) 조회에 성공했습니다.");
 
     private final String message;
 }
