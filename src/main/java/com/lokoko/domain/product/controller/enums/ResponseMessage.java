0e027b28085302c26ad72d786bb30a7b9b243870
huncozyboy <leejh5838@naver.com>
2025-07-05 23:33:54 +0900

    FEAT #31 : 상세조회 제품관련 조회 성공 메세지 추가

--- src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
@@ -12,6 +12,7 @@ public enum ResponseMessage {
     PRODUCT_CRAWL_NEW_SUCCESS("신상품 크롤링에 성공했습니다."),
     PRODUCT_OPTION_SUCCESS("상품 옵션 크롤링에 성공했습니다."),
     CATEGORY_NEW_LIST_SUCCESS("신상품 카테고리 리스트 반환에 성공했습니다."),
+    PRODUCT_DETAIL_SUCCESS("상세조회 (제품관련) 조회에 성공했습니다."),
     NAME_BRAND_SEARCH_SUCCESS("상품명 / 브랜드명 제품 검색에 성공했습니다."),
     PRODUCT_MIGRATION_SUCCESS("제품 검색 필드 업데이트 완료");
 
