84204e884dd403a8dbd4899c8d2aa7e7c6ff145c
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-05 22:56:52 +0900

    mod #28: @Bean 어노테이션 추가와 AmazonS3 → S3Client (AWS SDK v2)로 마이그레이션

--- src/main/java/com/lokoko/global/config/S3Config.java
+++ src/main/java/com/lokoko/global/config/S3Config.java
@@ -1,13 +1,14 @@
 package com.lokoko.global.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 
 @Configuration
@@ -27,14 +28,13 @@ public class S3Config {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public AmazonS3 getAmazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicAWSCredentials(accessKey, secretKey)
-                        )
-                )
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
