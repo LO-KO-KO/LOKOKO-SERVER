2f5eb9110097810c569a81fdd7277ec45e2c5241
huncozyboy <leejh5838@naver.com>
2025-07-06 22:02:40 +0900

    FEAT #44 : 카테고리별 인기 상품 조회 응답 dto 구현

--- src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/product/controller/enums/ResponseMessage.java
@@ -15,7 +15,8 @@ public enum ResponseMessage {
     PRODUCT_DETAIL_SUCCESS("상세조회 (제품관련) 조회에 성공했습니다."),
     PRODUCT_YOUTUBE_DETAIL_SUCCESS("상세조회 (유튜브) 조회에 성공했습니다."),
     NAME_BRAND_SEARCH_SUCCESS("상품명 / 브랜드명 제품 검색에 성공했습니다."),
-    PRODUCT_MIGRATION_SUCCESS("제품 검색 필드 업데이트 완료");
+    PRODUCT_MIGRATION_SUCCESS("제품 검색 필드 업데이트 완료"),
+    CATEGORY_POPULAR_LIST_SUCCESS("카테고리 인기 제품 리스트 반환에 성공했습니다.");
 
     private final String message;
 }
