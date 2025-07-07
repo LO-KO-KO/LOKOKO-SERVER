99d7450328b02ee85cbebdb97286668262a3d52c
huncozyboy <leejh5838@naver.com>
2025-07-08 04:46:20 +0900

    FEAT #48 : 페이지네이션 DTO 적용해서 무한 스크롤 반환

--- src/main/java/com/lokoko/domain/product/dto/CategoryProductPageResponse.java
+++ src/main/java/com/lokoko/domain/product/dto/CategoryProductPageResponse.java
@@ -2,9 +2,12 @@ package com.lokoko.domain.product.dto;
 
 import com.lokoko.global.common.response.PageableResponse;
 import java.util.List;
+import lombok.Builder;
 
-public record CategoryNewProductResponse(
+@Builder
+public record CategoryProductPageResponse(
         String searchQuery,
+        String parentCategoryName,
         List<ProductResponse> products,
         PageableResponse pageInfo
 ) {
