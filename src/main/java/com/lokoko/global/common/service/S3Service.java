ad6d0d4eaa8e4a0fd5e9419c8128a4ade1e159f7
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-09 01:59:11 +0900

    feat #39: S3Service 구현

---
+++ src/main/java/com/lokoko/global/common/service/S3Service.java
@@ -0,0 +1,45 @@
+package com.lokoko.global.common.service;
+
+import com.lokoko.domain.image.exception.FileTypeNotSupportedException;
+import com.lokoko.global.common.dto.PresignedUrlResponse;
+import com.lokoko.global.config.S3Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Presigner s3Presigner;
+    private final S3Config s3Config;
+
+    public PresignedUrlResponse generatePresignedUrl(String fileType) {
+        String uuid = UUID.randomUUID().toString();
+        String key;
+
+        if (fileType.startsWith("image")) {
+            key = "images/" + uuid + ".jpg";
+        } else if (fileType.startsWith("video")) {
+            key = "videos/" + uuid + ".mp4";
+        } else {
+            throw new FileTypeNotSupportedException();
+        }
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(s3Config.getBucket())
+                .key(key)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(builder -> builder
+                .putObjectRequest(putObjectRequest)
+                .signatureDuration(Duration.ofMinutes(10))
+        );
+
+        return new PresignedUrlResponse(presignedRequest.url().toString());
+    }
+}
