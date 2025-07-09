89f754727e2b257481a3f600eb21790809c14817
rookie <piresj0318@gmail.com>
2025-07-10 01:27:03 +0900

    MOD #51 : 상품명 / 브랜드명으로 상품 검색 기능에서 정렬 기능 구현
    상품명 / 브랜드명으로 상품 검색을 할때, 정렬이 되지 않았습니다. 리뷰 수 기준 내림차순, 별점 기준 내림차순 정렬이 이루어지도록 수정했습니다.

--- src/main/java/com/lokoko/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/lokoko/domain/product/repository/ProductRepositoryImpl.java
@@ -1,7 +1,5 @@
 package com.lokoko.domain.product.repository;
 
-import static com.lokoko.domain.product.entity.QProduct.product;
-
 import com.lokoko.domain.product.entity.Product;
 import com.lokoko.domain.product.entity.QProduct;
 import com.lokoko.domain.product.entity.enums.MiddleCategory;
@@ -38,61 +36,96 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     /**
      * 주어진 토큰 리스트를 기반으로 상품 검색 단계적으로 검색이 수행되고, 각 단계에서 결과가 존재하면 바로 반환
      *
-     * @param tokens 형태소 분석기를 통해서 ㅁ나들어진 검색어 토큰 리스트
+     * @param tokens 형태소 분석기를 통해서 만들어진 검색어 토큰 리스트
      * @return 검색 조건에 부합하는 Product List
      */
+
     @Override
     public Slice<Product> searchByTokens(List<String> tokens, Pageable pageable) {
         List<Product> allMatches;
         if (tokens.isEmpty()) {
             allMatches = List.of();
         } else {
+            // 리뷰 수 계산
+            NumberExpression<Long> reviewCount = r.id.count();
+
+            // 평점 평균 계산
+            NumberExpression<Integer> ratingNum = new CaseBuilder()
+                    .when(r.rating.eq(Rating.ONE)).then(1)
+                    .when(r.rating.eq(Rating.TWO)).then(2)
+                    .when(r.rating.eq(Rating.THREE)).then(3)
+                    .when(r.rating.eq(Rating.FOUR)).then(4)
+                    .when(r.rating.eq(Rating.FIVE)).then(5)
+                    .otherwise(0);
+
+            NumberExpression<Double> ratingAvgExpr = ratingNum.castToNum(Double.class).avg();
+
             String fullKeyword = String.join("", tokens);
             List<Product> exactMatches = queryFactory
-                    .selectFrom(product)
-                    .where(product.searchToken.containsIgnoreCase(fullKeyword))
+                    .select(p)
+                    .from(p)
+                    .leftJoin(r).on(r.product.eq(p))
+                    .where(p.searchToken.containsIgnoreCase(fullKeyword))
+                    .groupBy(p.id)
+                    .orderBy(reviewCount.desc(), ratingAvgExpr.desc())
                     .fetch();
             if (!exactMatches.isEmpty()) {
                 allMatches = exactMatches;
             } else {
                 BooleanBuilder allTokensMatch = new BooleanBuilder();
                 tokens.forEach(token ->
-                        allTokensMatch.and(product.searchToken.containsIgnoreCase(token))
+                        allTokensMatch.and(p.searchToken.containsIgnoreCase(token))
                 );
                 List<Product> allTokenMatches = queryFactory
-                        .selectFrom(product)
+                        .select(p)
+                        .from(p)
+                        .leftJoin(r).on(r.product.eq(p))
                         .where(allTokensMatch)
+                        .groupBy(p.id)
+                        .orderBy(reviewCount.desc(), ratingAvgExpr.desc())
                         .fetch();
                 if (!allTokenMatches.isEmpty()) {
                     allMatches = allTokenMatches;
                 } else if (tokens.size() >= 3) {
                     BooleanBuilder majorTokensMatch = new BooleanBuilder()
-                            .and(product.searchToken.containsIgnoreCase(tokens.get(0)))
-                            .and(product.searchToken.containsIgnoreCase(tokens.get(tokens.size() - 1)));
+                            .and(p.searchToken.containsIgnoreCase(tokens.get(0)))
+                            .and(p.searchToken.containsIgnoreCase(tokens.get(tokens.size() - 1)));
                     List<Product> majorMatches = queryFactory
-                            .selectFrom(product)
+                            .select(p)
+                            .from(p)
+                            .leftJoin(r).on(r.product.eq(p))
                             .where(majorTokensMatch)
+                            .groupBy(p.id)
+                            .orderBy(reviewCount.desc(), ratingAvgExpr.desc())
                             .fetch();
                     if (!majorMatches.isEmpty()) {
                         allMatches = majorMatches;
                     } else {
                         BooleanBuilder anyTokenMatch = new BooleanBuilder();
                         tokens.forEach(token ->
-                                anyTokenMatch.or(product.searchToken.containsIgnoreCase(token))
+                                anyTokenMatch.or(p.searchToken.containsIgnoreCase(token))
                         );
                         allMatches = queryFactory
-                                .selectFrom(product)
+                                .select(p)
+                                .from(p)
+                                .leftJoin(r).on(r.product.eq(p))
                                 .where(anyTokenMatch)
+                                .groupBy(p.id)
+                                .orderBy(reviewCount.desc(), ratingAvgExpr.desc())
                                 .fetch();
                     }
                 } else {
                     BooleanBuilder anyTokenMatch = new BooleanBuilder();
                     tokens.forEach(token ->
-                            anyTokenMatch.or(product.searchToken.containsIgnoreCase(token))
+                            anyTokenMatch.or(p.searchToken.containsIgnoreCase(token))
                     );
                     allMatches = queryFactory
-                            .selectFrom(product)
+                            .select(p)
+                            .from(p)
+                            .leftJoin(r).on(r.product.eq(p))
                             .where(anyTokenMatch)
+                            .groupBy(p.id)
+                            .orderBy(reviewCount.desc(), ratingAvgExpr.desc())
                             .fetch();
                 }
             }
