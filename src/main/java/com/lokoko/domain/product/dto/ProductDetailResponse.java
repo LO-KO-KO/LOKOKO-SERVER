57438d4a0d67a43fc846b93c0a1e30e5eebd5706
rookie <piresj0318@gmail.com>
2025-07-10 01:29:19 +0900

    MOD #51 : 제품 상세 정보를 조회할 때 플랫하게 데이터를 반환하도록 수정
    기존에는 products 에 ProductResponse 의 리스트를 담아서 보냈습니다. 클라이언트 쪽 요구에 따라, products 를 반환하는 것이 아닌, 제품 상세 정보를 플랫하게 반환하는 방식으로 수정했습니다.

--- src/main/java/com/lokoko/domain/product/dto/ProductDetailResponse.java
+++ src/main/java/com/lokoko/domain/product/dto/ProductDetailResponse.java
@@ -1,19 +1,46 @@
 package com.lokoko.domain.product.dto;
 
+import com.lokoko.domain.product.entity.Product;
 import com.lokoko.domain.product.entity.enums.MiddleCategory;
 import com.lokoko.domain.product.entity.enums.SubCategory;
 import java.util.List;
 
 public record ProductDetailResponse(
-        List<ProductResponse> products,
+
+        Long productId, // 제품 id(추후 상세조회를 위해서)
+        List<String> imageUrls, // 제품 이미지
+        String productName,// 제품 이름
+        String brandName, // 브랜드 이름
+        String unit, // 제품 단위
+        Long reviewCount, // 리뷰 개수
+        Double rating, // 별점
         List<String> productOptions,
         long normalPrice,
         String productDetail,
         String ingredients,
-        String shippingInfo,
         String oliveYoungUrl,
         String q10Url,
         MiddleCategory middleCategory,
         SubCategory subCategory
 ) {
+    public static ProductDetailResponse from(ProductResponse response, List<String> productOptions, Product product) {
+
+        return new ProductDetailResponse(
+                response.productId(),
+                response.imageUrls(),
+                response.productName(),
+                response.brandName(),
+                response.unit(),
+                response.reviewCount(),
+                response.rating(),
+                productOptions,
+                product.getNormalPrice(),
+                product.getProductDetail(),
+                product.getIngredients(),
+                product.getOliveYoungUrl(),
+                product.getQoo10Url(),
+                product.getMiddleCategory(),
+                product.getSubCategory()
+        );
+    }
 }
