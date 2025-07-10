ff92506c930e4e43310534a42b2f508ac7c46418
rookie <piresj0318@gmail.com>
2025-07-10 16:17:15 +0900

    FEAT #60 : 유저 관련 예외 추가

---
+++ src/main/java/com/lokoko/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.lokoko.domain.user.exception;
+
+import com.lokoko.global.common.exception.BaseException;
+import org.springframework.http.HttpStatus;
+
+public class UserNotFoundException extends BaseException {
+
+    public UserNotFoundException() {
+        super(HttpStatus.NOT_FOUND, ErrorMessage.USER_NOT_FOUND.getMessage());
+    }
+
+}
