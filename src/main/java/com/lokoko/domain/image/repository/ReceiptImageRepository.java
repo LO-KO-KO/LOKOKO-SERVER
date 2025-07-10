03f5174ab9931aabc3f7d23ef30014653922539a
rookie <piresj0318@gmail.com>
2025-07-10 16:19:19 +0900

    FEAT #60 : 리뷰 삭제시 연관된 영상, 사진, 영수증 삭제 처리 구현

--- src/main/java/com/lokoko/domain/image/repository/ReceiptImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ReceiptImageRepository.java
@@ -1,9 +1,11 @@
 package com.lokoko.domain.image.repository;
 
 import com.lokoko.domain.image.entity.ReceiptImage;
+import com.lokoko.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReceiptImageRepository extends JpaRepository<ReceiptImage, Long> {
+    void deleteAllByReview(Review review);
 }
