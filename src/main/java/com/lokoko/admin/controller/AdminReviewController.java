cbe05152867159f4cfb2cefa98ee5a3573c60f1d
rookie <piresj0318@gmail.com>
2025-07-10 16:22:46 +0900

    FEAT #60 : 어드민 리뷰 컨트롤러 구현

---
+++ src/main/java/com/lokoko/admin/controller/AdminReviewController.java
@@ -0,0 +1,24 @@
+package com.lokoko.admin.controller;
+
+import com.lokoko.admin.service.AdminReviewService;
+import com.lokoko.global.auth.annotation.CurrentUser;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "ADMIN")
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminReviewController {
+
+    private final AdminReviewService adminReviewService;
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public void deleteReviewByAdmin(@CurrentUser Long userId, @PathVariable Long reviewId) {
+        adminReviewService.deleteReview(userId, reviewId);
+    }
+}
