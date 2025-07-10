879846f82d2fb70519b017372569229bc1557273
huncozyboy <leejh5838@naver.com>
2025-07-11 01:04:21 +0900

    MOD #58 : 소셜 로그인 비즈니스 로직 수정

--- src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
+++ src/main/java/com/lokoko/global/auth/line/dto/LineLoginResponse.java
@@ -1,8 +1,26 @@
 package com.lokoko.global.auth.line.dto;
 
 import com.lokoko.global.auth.entity.enums.OauthLoginStatus;
+import com.lokoko.global.auth.jwt.dto.LoginDto;
 
 public record LineLoginResponse(
+        String accessToken,
+        String refreshToken,
         OauthLoginStatus loginStatus
 ) {
+    public static LineLoginResponse of(
+            String accessToken,
+            String refreshToken,
+            OauthLoginStatus loginStatus
+    ) {
+        return new LineLoginResponse(accessToken, refreshToken, loginStatus);
+    }
+
+    public static LineLoginResponse from(LoginDto loginDto) {
+        return new LineLoginResponse(
+                loginDto.accessToken(),
+                loginDto.refreshToken(),
+                loginDto.loginStatus()
+        );
+    }
 }
