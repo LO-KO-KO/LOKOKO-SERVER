f61d4dff0e90955ad921cfca69250a8ae135e470
rookie <piresj0318@gmail.com>
2025-07-10 16:19:45 +0900

    FEAT #60 : 리뷰 관련 예외 메시지 추가

--- src/main/java/com/lokoko/domain/review/exception/ErrorMessage.java
+++ src/main/java/com/lokoko/domain/review/exception/ErrorMessage.java
@@ -9,7 +9,9 @@ public enum ErrorMessage {
     RATING_NOT_FOUND("존재하지 않는 평점입니다."),
     INVALID_MEDIA_TYPE_FORMAT("mediaType은 'video/xxx' 또는 'image/xxx' 형식이어야 합니다."),
     INVALID_MEDIA_TYPE_PLURAL("mediaType은 'videos' 또는 'images'가 아닌 'video/' 또는 'image/'로 시작해야 합니다."),
-    UNSUPPORTED_MEDIA_TYPE("지원하지 않는 파일 형식입니다.");
+    UNSUPPORTED_MEDIA_TYPE("지원하지 않는 파일 형식입니다."),
+    REVIEW_NOT_FOUND("존재하지 않는 리뷰입니다.");
+
 
     private final String message;
 }
