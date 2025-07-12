acea3503b979b0a49c048a65815ddc76426c2dc0
huncozyboy <leejh5838@naver.com>
2025-07-12 17:33:19 +0900

    FEAT #77 : 리뷰 좋아요 컨트롤러 구현

--- src/main/java/com/lokoko/domain/like/controller/ReviewLikeController.java
+++ src/main/java/com/lokoko/domain/like/controller/ReviewLikeController.java
@@ -1,7 +1,18 @@
 package com.lokoko.domain.like.controller;
 
+import static com.lokoko.domain.like.controller.enums.ResponseMessage.REVIEW_LIKE_TOGGLE_SUCCESS;
+
+import com.lokoko.domain.like.dto.response.ReviewLikeResponse;
+import com.lokoko.domain.like.service.ReviewLikeService;
+import com.lokoko.global.auth.annotation.CurrentUser;
+import com.lokoko.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/likes/reviews/{reviewId}")
 @RequiredArgsConstructor
 public class ReviewLikeController {
+    private final ReviewLikeService reviewLikeService;
+
+    @Operation(summary = "리뷰 좋아요 토글 (추가/취소)")
+    @PostMapping
+    public ApiResponse<ReviewLikeResponse> toggleLike(@PathVariable final Long reviewId,
+                                                      @Parameter(hidden = true) @CurrentUser Long userId) {
+        long likeCount = reviewLikeService.toggleReviewLike(reviewId, userId);
+
+        return ApiResponse.success(HttpStatus.OK, REVIEW_LIKE_TOGGLE_SUCCESS.getMessage(),
+                new ReviewLikeResponse(likeCount));
+    }
 }
--- src/main/java/com/lokoko/domain/like/controller/enums/ResponseMessage.java
+++ src/main/java/com/lokoko/domain/like/controller/enums/ResponseMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResponseMessage {
-    PRODUCT_LIKE_TOGGLE_SUCCESS("상품 좋아요 토글에 성공했습니다.");
+    PRODUCT_LIKE_TOGGLE_SUCCESS("상품 좋아요 토글에 성공했습니다."),
+    REVIEW_LIKE_TOGGLE_SUCCESS("리뷰 좋아요 토글에 성공했습니다.");
 
     private final String message;
 }
