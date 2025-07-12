41525bd93b04206ea7b5d4325cc05d8cbe099b4a
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-12 18:35:39 +0900

    mod #69 : 로직 수정

--- src/main/java/com/lokoko/domain/image/repository/ReviewImageRepository.java
+++ src/main/java/com/lokoko/domain/image/repository/ReviewImageRepository.java
@@ -3,21 +3,9 @@ package com.lokoko.domain.image.repository;
 import com.lokoko.domain.image.entity.ReviewImage;
 import com.lokoko.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long>, ReviewImageRepositoryCustom {
-
-    @Query("""
-                SELECT ri FROM ReviewImage ri
-                JOIN FETCH ri.review r         
-                JOIN FETCH r.product p       
-                WHERE ri.displayOrder = 0      
-            """)
-    List<ReviewImage> findAllMainReviewImageWithReview();
-
     void deleteAllByReview(Review review);
 }
--- src/main/java/com/lokoko/domain/image/repository/ReviewImageRepositoryImpl.java
+++ src/main/java/com/lokoko/domain/image/repository/ReviewImageRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.lokoko.domain.image.repository;
 
 import com.lokoko.domain.image.entity.QReviewImage;
 import com.lokoko.domain.image.entity.ReviewImage;
+import com.lokoko.domain.like.entity.QReviewLike;
 import com.lokoko.domain.product.entity.QProduct;
 import com.lokoko.domain.review.entity.QReview;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -18,6 +19,7 @@ public class ReviewImageRepositoryImpl implements ReviewImageRepositoryCustom {
     private static final QReviewImage reviewImage = QReviewImage.reviewImage;
     private static final QReview review = QReview.review;
     private static final QProduct product = QProduct.product;
+    private static final QReviewLike reviewLike = QReviewLike.reviewLike;
 
     @Override
     public List<ReviewImage> findMainImageReviewSorted() {
@@ -25,11 +27,13 @@ public class ReviewImageRepositoryImpl implements ReviewImageRepositoryCustom {
                 .selectFrom(reviewImage)
                 .join(reviewImage.review, review).fetchJoin()
                 .join(review.product, product).fetchJoin()
+                .leftJoin(reviewLike).on(reviewLike.review.eq(review))
                 // 대표 이미지 조건: displayOrder == 0
                 .where(reviewImage.displayOrder.eq(0))
+                .groupBy(reviewImage.id, review.id, product.id, review.rating)
                 // 정렬 조건: 좋아요 내림차순 → 평점 내림차순
                 .orderBy(
-                        review.likeCount.desc(),
+                        reviewLike.count().desc(),
                         review.rating.desc()
                 )
                 .limit(4)
